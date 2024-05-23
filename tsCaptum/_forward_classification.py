3d19a0dba24cb8f2808774c3bea9c0e626ff3270
davide-serramazza <33940197+davide-serramazza@users.noreply.github.com>
2024-05-23 12:52:07 +0100

    Delete tsCaptum/_forward_classification.py
    removing old files

--- tsCaptum/_forward_classification.py
+++
@@ -1,8 +0,0 @@
-def forward_classification(X_test : torch.Tensor, model):
-	# convert X to pytorch tensor
-	X_test_numpy = X_test.detach().numpy()
-	# compute probability
-	predictions = model.predict_proba(X_test_numpy)
-	# return result as torch tensor as expected by captum attribution method
-	return torch.tensor(predictions)
-
